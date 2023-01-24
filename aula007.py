58b1da06ec4dd9499c9969039c8be3dcb6a3b9c2
Beacesconetto <you@example.com>
2023-01-24 15:31:09 -0300

    aula 7b

---
+++ aula007.py
@@ -0,0 +1,7 @@
+ # from math import sqrt, floor
+# num = int(input('Digite um número: '))
+# raiz = sqrt(num)
+# print('A raiz de {} é igual a {}' .format(num,floor(raiz)))
+import random
+num = random.randint(1,10)
+print('O seu numero é {} ' .format(num))
