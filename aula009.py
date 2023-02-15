d69b8c8e4969584de9adac2a86ef6c89bb87b173
Beacesconetto <you@example.com>
2023-02-15 14:13:33 -0300

    aulas09

---
+++ aula009.py
@@ -0,0 +1,11 @@
+frase = 'Curso em Vídeo Python'
+dividido = frase.split()
+print(len(dividido[0]))
+
+
+# se quiser escrever um texto inteiro sem usar varios print
+#print("""Welcome! Are you completely new to programming?
+#about why and how to get started with Python. Fortunately 
+#an experienced prgrammer in any programming language 
+#(whatever it may be) can pick up Python very quickly.
+#Its also easy for beginners to use and learn, so jump in!""")
