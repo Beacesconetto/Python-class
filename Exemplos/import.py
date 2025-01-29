89e28738ebe388f675699a40eb27139f10c1c9af
Beacesconetto <beacesconetto203@hotmail.com>
2025-01-29 14:56:27 -0300

    modulo personalizado

--- Exemplos/import.py
+++ Exemplos/import.py
@@ -1,6 +1,7 @@
 import random
 import datetime
 import math
+from modulo_personalizado import saudar
 
 resultado = math.sqrt(25)
 print(resultado)  
@@ -10,7 +11,12 @@ print(numero_aleatorio)
 
 
 data_atual = datetime.datetime.now()
-print(data_atual)  
+print(data_atual) 
+
+saudar("João")
+# seria assim se usasse o import geral -> resultado = modulo_personalizado.calcular_soma(5, 3)
+# print(resultado)
+
 
 """
 Também podemos importar funções específicas de um módulo utilizando a sintaxe from módulo import função.
