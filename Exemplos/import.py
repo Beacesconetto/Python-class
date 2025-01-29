edcbc2ec26ef12837c96afa23bb64d9cbc0f1802
Beacesconetto <beacesconetto203@hotmail.com>
2025-01-29 14:19:28 -0300

    importacao de módulos

---
+++ Exemplos/import.py
@@ -0,0 +1,24 @@
+import random
+import datetime
+import math
+
+resultado = math.sqrt(25)
+print(resultado)  
+
+numero_aleatorio = random.randint(1, 10)
+print(numero_aleatorio) 
+
+
+data_atual = datetime.datetime.now()
+print(data_atual)  
+
+"""
+Também podemos importar funções específicas de um módulo utilizando a sintaxe from módulo import função.
+
+from math import sqrt
+
+
+resultado = sqrt(25)
+print(resultado) 
+
+"""
--- Exemplos/ler-arquivos.py
+++ Exemplos/ler-arquivos.py
@@ -12,3 +12,9 @@ arquivo.close()
 arquivo = open("dado.txt", "w")
 arquivo.write("Ola, mundo!")
 arquivo.close()
+
+# Você também pode utilizar a declaração with para manejar a abertura e fechamento de arquivos de maneira automática.
+
+with open("dados.txt", "r") as arquivo:
+    conteudo = arquivo.read()
+    print(conteudo)
