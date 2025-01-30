216284b516d928f54f3b0ff5b0a5705393400d5e
Beacesconetto <beacesconetto203@hotmail.com>
2025-01-30 15:12:27 -0300

    Exercicio 26

---
+++ Exercicios_Python/ex026.py
@@ -0,0 +1,11 @@
+"""
+Faça um programa que leia uma frase pelo teclado e mostra:
+Quantas vezes aparece a letra A
+Em que posição ela aparece a primeira vez
+Em que posição ela aparece a última vez
+"""
+
+frase = str(input("Digite uma frase: ")).lower()
+print("A letra A aparece {} vezes na frase" .format(frase.count("a")))
+print("A primeira letra A apareceu na posição {}" .format(frase.find("a")+1))
+print("A última letra A apareceu na posição {}" .format(frase.rfind("a")+1))
