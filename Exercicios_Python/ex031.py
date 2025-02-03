c9dafcdcbcd725d95a136beb530b23181058551c
Beacesconetto <beacesconetto203@hotmail.com>
2025-02-03 17:26:44 -0300

    exercicio ano bissexto

--- Exercicios_Python/ex031.py
+++ Exercicios_Python/ex031.py
@@ -1,8 +1,13 @@
-vel = float(input('Qual é a velocidade atual do carro? '))
+vel = float(input('Qual é a distância da sua viagem? '))
+print('Você está prestes a começar uma viagem de {:.2f}Km/h'.format(vel))
 
 if vel <=200:
-    print('Você está prestes a começar uma viagem de {:.2f}Km/h'.format(vel))
     print('E o preço da sua passagem será de R${:.2f}'.format(vel*0.50))
 else:
-    print('Você está prestes a começar uma viagem de {:.2f}Km/h'.format(vel))
     print('E o preço da sua passagem será de R${:.2f}'.format(vel*0.45))
+
+"""
+Maneira simplificada
+preço = vel * 0.50 if vel <= 200 else vel * 0.45
+print('E o preço da sua passagem será de R${:.2f}'.format(preço))
+"""
