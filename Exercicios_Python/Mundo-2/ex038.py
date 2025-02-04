e0fd76e01223b4342f7054c14f81ab950279c996
Beacesconetto <beacesconetto203@hotmail.com>
2025-02-04 17:34:43 -0300

    exercicio comparação de números

---
+++ Exercicios_Python/Mundo-2/ex038.py
@@ -0,0 +1,9 @@
+a = int(input('Digite um número: '))
+b = int(input('Digite outro número: '))
+
+if a> b:
+    print(f'{a} é maior que {b}')
+elif b > a:
+    print(f'{b} é maior {a}')
+else:
+    print('Os dois valores são iguais')         
