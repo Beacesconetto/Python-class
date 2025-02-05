a89ccff116ffbb711e77d2650c6e5dde752e4aee
Beacesconetto <beacesconetto203@hotmail.com>
2025-02-05 17:14:03 -0300

    exercicio contagem regressiva

--- Aulas/aula10.py
+++
@@ -1,6 +0,0 @@
-nome = str(input('Qual é o seu nome? '))
-if nome == 'Beatriz':
-    print('Que nome lindo você tem!')
-else:
-    print('Seu nome é tão normal!') 
-print('Bom dia, {}!'.format(nome))
---
+++ Aulas/aula13.py
@@ -0,0 +1,7 @@
+for c in range (6, -1, -1):
+    print(c)
+print('Fim')
+
+for c in range (0, 3):
+    n = int(input('Digite um valor: '))
+print('Fim')
