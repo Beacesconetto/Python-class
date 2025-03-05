ec335e8e7878ab6f00a4ab6313c162caf1a68439
Beacesconetto <beacesconetto203@hotmail.com>
2025-03-05 14:55:02 -0300

    feat: factorial

---
+++ Exercicios_Python/Mundo-2/ex060.py
@@ -0,0 +1,14 @@
+from math import factorial
+
+num = int(input('Digite um número para calcular seu fatorial: '))
+c = num
+f = 1
+print(f'{num}! = ', end='')
+while c > 0:
+    print(f'{c}', end='')
+    print(' x ' if c > 1 else ' = ', end='')
+    f *= c
+    c -= 1
+print(f'{f}')    
+# f = factorial(num)
+# print(f'O fatorial de {num} é {f}')
