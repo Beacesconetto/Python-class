d5e2aa7de9e05104455ce419b2eb42314ca1d9ad
Beacesconetto <beacesconetto203@hotmail.com>
2025-03-05 14:55:56 -0300

    feat: factorial

--- Exercicios_Python/Mundo-2/ex060.py
+++ Exercicios_Python/Mundo-2/ex060.py
@@ -9,6 +9,8 @@ while c > 0:
     print(' x ' if c > 1 else ' = ', end='')
     f *= c
     c -= 1
-print(f'{f}')    
-# f = factorial(num)
-# print(f'O fatorial de {num} é {f}')
+print(f'{f}')
+
+''' Outra forma de fazer mais simples
+f = factorial(num)
+print(f'O fatorial de {num} é {f}') '''
