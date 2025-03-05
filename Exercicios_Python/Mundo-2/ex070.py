8c3b41c124139c11d3e4e3e371ddfd30eb23d5eb
Beacesconetto <beacesconetto203@hotmail.com>
2025-03-05 15:10:29 -0300

    feat: estatistica em produtos

---
+++ Exercicios_Python/Mundo-2/ex070.py
@@ -0,0 +1,23 @@
+print('-' * 40)
+print('           LOJA SUPER BARATÃO')
+print('-' * 40)
+total = caro = menor = cont = 0
+barato = ''
+while True:
+    produto = str(input('Nome do prouto: ')).strip()
+    preco = float(input('Preço: R$'))
+    total += preco
+    cont += 1
+    resp = ' '
+    if preco >= 1000:
+        caro += 1
+    if cont == 1 or preco < meno:  
+        menor = preco
+        barato = produto
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N] ')).strip().upper()[0]
+    if resp == 'N':
+        print(f'O total da compra foi R${total:.2f}')
+        print(f'Temos {caro} produtos custando mais de R$1000.00')
+        print(f'O produto mais barato foi {barato} que custa R${menor}')
+        break
